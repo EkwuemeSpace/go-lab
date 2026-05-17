13016c29a0167e8700d75d15d130a26ee7227ce7
EkwuemeSpace <innocentekwueme79@gmail.com>
2026-05-17 11:17:07 +0100

    updated

--- Palindrome_Application/reverseString.go
+++ Palindrome_Application/reverseString.go
@@ -6,7 +6,7 @@ func reverseInput(input string) string {
 	runes := []rune(input)
 
 	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[i], runes[i]
+		runes[i], runes[j] = runes[j], runes[i]
 	}
 	return strings.ReplaceAll(string(runes), " ", "")
 }
