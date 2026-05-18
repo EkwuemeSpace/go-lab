b6ff7cfccb217d5257426ae41e3339b9ba4dbc84
EkwuemeSpace <innocentekwueme79@gmail.com>
2026-05-18 07:12:36 +0100

    updated the programe to reject empty input

--- Anagram_Detector/programme_files/validateInput.go
+++ Anagram_Detector/programme_files/validateInput.go
@@ -1,11 +1,18 @@
 package main
 
 func validatorInput(first, second string) error {
-	if err := validator(first); err != nil {
+	if err := validateChar(first); err != nil {
 		return err
 	}
-	if err := validator(second); err != nil {
+	if err := validateChar(second); err != nil {
 		return err
 	}
 	return nil
+}
+
+func isEmptyInput(first, second string) bool {
+	if !(first == "" || second == "") {
+		return true
+	}
+	return false
 }
